337bb6ce3bd89072155d6abeaec6b589845690c1
kohantikanath <kohan.nath@gmail.com>
2024-07-03 11:06:35 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,6 +11,43 @@ app.listen(port, () => {
   console.log(`listening on http:/${port}`)
 })
 
-// app.get('/courses', (req, res) =>{
-//     res.json(courses)
+let courses = [
+    { id: 1, name:"java"},
+    { id: 2, name:"javaScript"},
+    { id: 3, name:"Node"}
+]
+
+app.get('/courses', (req, res) =>{
+    res.json(courses)
+});
+
+app.post('/courses',(req, res) =>{
+    let singleCourse = {
+        id: courses.length+1,
+        name: req.body.name
+    }
+    courses.push(singleCourse);
+    res.json(singleCourse);
+})
+
+app.put('/courses/:id', (req, res) =>{
+  try{
+    let singleCourse = courses.find((course) => {
+      return course.id === +req.params.id
+    })
+
+    if(!singleCourse){
+      res.status(404).send('course does not exist');
+    }
+
+    singleCourse.name = req.body.name;
+    res.send(courses);
+  } catch (err){
+    res.status(500).send(err);
+  }
+})
+
+
+// app.listen(port, () =>{
+//     console.log("server started");
 // });
